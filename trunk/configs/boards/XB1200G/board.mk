9863f38386ef3e5626f1152531de36db5f82932c
sseseed <91378229+sseseed@users.noreply.github.com>
2021-09-30 11:55:13 +0800

    Update board.mk

--- trunk/configs/boards/XB1200G/board.mk
+++ trunk/configs/boards/XB1200G/board.mk
@@ -3,6 +3,6 @@
 ##################################################################
 # XB1200G       # XB1200G              # MT7621  #
 ##################################################################
-#CFLAGS += -DBOARD_XB1200G -DBOARD_MT7615_DBDC
-CFLAGS += -DBOARD_XB1200G
+
+CFLAGS += -DBOARD_XB1200G -DBOARD_MT7615_DBDC
 BOARD_NUM_USB_PORTS=0
