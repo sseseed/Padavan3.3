8e15dac98baa6cd0b7ad34366dd50debc2b31389
sseseed <91378229+sseseed@users.noreply.github.com>
2021-10-03 22:52:55 +0800

    Add files via upload

---
+++ trunk/configs/boards/board.h
@@ -0,0 +1,33 @@
+/* GE-AP */
+
+#define BOARD_PID		"GE-AP"
+#define BOARD_NAME		"GE-AP"
+#define BOARD_DESC		"GE-AP Wireless Router"
+#define BOARD_VENDOR_NAME	"GE-AP"
+#define BOARD_VENDOR_URL	"http://www.mediatek.cn"
+#define BOARD_MODEL_URL		"http://www.mediatek.cn"
+#define BOARD_BOOT_TIME		60
+#define BOARD_FLASH_TIME	180
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	18
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
+#define BOARD_GPIO_LED_POWER	3
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
