bfa15ef842ea9531af5fdd4f95693bd32a2e6fba
sseseed <91378229+sseseed@users.noreply.github.com>
2021-10-03 22:36:40 +0800

    Create board.h

---
+++ trunk/configs/boards/XB764(7628-7612)FE-AP/board.h
@@ -0,0 +1,31 @@
+/* FE-AP */
+
+#define BOARD_PID		"FE-AP"
+#define BOARD_NAME		"FE-AP"
+#define BOARD_DESC		"FE-AP Wireless Router"
+#define BOARD_VENDOR_NAME	"FE-AP"
+#define BOARD_VENDOR_URL	"https://www.mediatek.cn/"
+#define BOARD_MODEL_URL		"https://www.mediatek.cn/"
+#define BOARD_BOOT_TIME		60
+#define BOARD_FLASH_TIME		180
+#define BOARD_GPIO_BTN_RESET	38
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#define BOARD_GPIO_LED_WIFI 44
+#undef  BOARD_GPIO_LED_SW2G	
+#undef  BOARD_GPIO_LED_POWER
+#define BOARD_GPIO_LED_LAN  41
+#define BOARD_GPIO_LED_WAN  39
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
