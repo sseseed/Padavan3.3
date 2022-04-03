b513fdab440b34a5835b3d2dd057c31688c876fd
sseseed <91378229+sseseed@users.noreply.github.com>
2022-04-03 20:26:17 +0800

    Create board.h

---
+++ trunk/configs/boards/GL-FE-AP/board.h
@@ -0,0 +1,31 @@
+/* GL-FE-AP */
+
+#define BOARD_PID		"GL-FE-AP"
+#define BOARD_NAME		"GL-FE-AP"
+#define BOARD_DESC		"GL-FE-AP Wireless Router"
+#define BOARD_VENDOR_NAME	"GL-FE-AP"
+#define BOARD_VENDOR_URL	"https://www.mediatek.cn/"
+#define BOARD_MODEL_URL		"https://www.mediatek.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME        120
+#define BOARD_GPIO_BTN_RESET	36
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define  BOARD_GPIO_LED_SW2G	44
+#define  BOARD_GPIO_LED_POWER 11
+#define BOARD_GPIO_LED_LAN  42
+#undef BOARD_GPIO_LED_WAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef BOARD_HAS_5G_11AC
+#undef BOARD_NUM_ANT_5G_TX
+#undef BOARD_NUM_ANT_5G_RX
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
