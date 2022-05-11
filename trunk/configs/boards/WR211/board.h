9bdc07fdc3d50e2057c112eb3d38d9fbacd8fab7
sseseed <91378229+sseseed@users.noreply.github.com>
2022-05-11 09:13:54 +0800

    Update board.h

--- trunk/configs/boards/WR211/board.h
+++ trunk/configs/boards/WR211/board.h
@@ -7,17 +7,17 @@
 #define BOARD_VENDOR_URL	"https://www.mediatek.cn/"
 #define BOARD_MODEL_URL		"https://www.mediatek.cn/"
 #define BOARD_BOOT_TIME		30
-#define BOARD_FLASH_TIME        120
-#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_FLASH_TIME        65
+#define BOARD_GPIO_BTN_RESET	3
 #undef  BOARD_GPIO_BTN_FN1
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_BTN_ROUTER
 #undef  BOARD_GPIO_LED_ALL
-#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_WIFI 0
 #undef  BOARD_GPIO_LED_SW2G	
-#undef  BOARD_GPIO_LED_POWER
-#define BOARD_GPIO_LED_LAN  41
-#define BOARD_GPIO_LED_WAN  39
+#define BOARD_GPIO_LED_POWER  4
+#define BOARD_GPIO_LED_LAN  2
+#undef  BOARD_GPIO_LED_WAN
 #undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
 #undef  BOARD_GPIO_PWR_USB
