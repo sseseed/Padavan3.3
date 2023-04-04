9bd14d9b5e13f73732aae0df2303163d03034e6a
sseseed <91378229+sseseed@users.noreply.github.com>
2023-04-04 21:00:48 +0800

    Create board.h

---
+++ trunk/configs/boards/ZCM-MT7688/board.h
@@ -0,0 +1,31 @@
+/* GL-FE-AP */
+
+#define BOARD_PID		"ZCM-MT7688"
+#define BOARD_NAME		"ZCM-MT7688"
+#define BOARD_DESC		"ZCM-MT7688 Wireless Router"
+#define BOARD_VENDOR_NAME	"ZCM-MT7688"
+#define BOARD_VENDOR_URL	"https://www.mediatek.cn/"
+#define BOARD_MODEL_URL		"https://www.mediatek.cn/"
+#define BOARD_BOOT_TIME 30
+#define BOARD_FLASH_TIME  65
+#define BOARD_GPIO_BTN_RESET  38
+#undef  BOARD_GPIO_BTN_FN1
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_BTN_ROUTER
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define  BOARD_GPIO_LED_SW2G 44
+#undef  BOARD_GPIO_LED_POWER
+#define BOARD_GPIO_LED_LAN  42
+#define BOARD_GPIO_LED_WAN 43
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
+#undef BOARD_HAS_5G_11AC
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	1
+#define BOARD_NUM_ANT_2G_RX	1
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
